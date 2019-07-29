010d02fa243807ce20ba5776e527005eda813655
Jose Zorrilla <jzorrilla@x-red.com>
2019-07-30 01:29:02 +1000

    Agrego compatibilidad con mysqli

--- uf.php
+++ uf.php
@@ -25,14 +25,14 @@ if (file_exists(dirname(__FILE__) . "/config.php")) {
 // Este archivo sobre escribe las variables de config.php
 if (file_exists(dirname(__FILE__) . "/config_local.php")) {
         include(dirname(__FILE__) . "/config_local.php");
-} 
+}
 
 // Define el timezone
 date_default_timezone_set('America/Santiago');
 
 // Conexion a la base de datos
-mysql_connect($DB_SERVIDOR,$DB_USUARIO,$DB_CLAVE) or die("Imposible conectarse al servidor.");
-mysql_select_db($DB_BASE) or die("Imposible abrir Base de datos");
+$con = mysqli_connect($DB_SERVIDOR,$DB_USUARIO,$DB_CLAVE) or die("Imposible conectarse al servidor.");
+mysqli_select_db($con, $DB_BASE) or die("Imposible abrir Base de datos");
 
 $fecha = date_create($argv[1]);
 
@@ -49,9 +49,9 @@ while (!$fin) {
 		$json = json_decode($source);
 
 		if (sizeof($json->serie)!=0) {
-				$sql = "insert ignore into uf (uf_fecha,uf_valor,uf_fechaact) values ('" . 
+				$sql = "insert ignore into uf (uf_fecha,uf_valor,uf_fechaact) values ('" .
 					date_format($fecha,'Y-m-d') . "'," . $json->serie[0]->valor .",now())";
-				$con=mysql_query($sql);
+				mysqli_query($con, $sql);
 		}
 		else
 			$fin =1;
